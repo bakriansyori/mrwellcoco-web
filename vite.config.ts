481a6be74d44889be9355045d3f4e9ebc7a9f37f
bakriansyori <bakriansyori@gmail.com>
2026-04-25 00:19:34 +0700

    Update vite.config.ts
    // Tambahkan base path relatif agar file JS/CSS terpanggil dengan benar
      base: "./",

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  // Tambahkan base path relatif agar file JS/CSS terpanggil dengan benar
+  base: "./", 
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
